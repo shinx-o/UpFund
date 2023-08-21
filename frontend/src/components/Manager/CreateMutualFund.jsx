7685a43720072d90d5a92601cafd436c2dc13f9f
shinix-o <ayushchamp20@gmail.com>
2023-08-21 11:41:58 +0530

    csv data

--- frontend/src/components/Manager/CreateMutualFund.jsx
+++ frontend/src/components/Manager/CreateMutualFund.jsx
@@ -1,20 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import './CreateMutualFund.scss'
-import Form from 'react-bootstrap/Form';
-import Table from 'react-bootstrap/Table';
-import { BsXCircleFill } from "react-icons/bs";
-import { Link } from 'react-router-dom';
 import { getAllStocks } from '../DataFetch/fetchManagerData';
 
 
 
 
 export default function CreateMutualFund() {
-    const [passwordVisible, setPasswordVisible] = useState(false);
-    const [stocksList, setStocksList] = useState([]);
-    const [addStock, setAddStocks] = useState([]);
-    const [enableButton, setEnableButton] = useState(false);
-    const [corpus, setCorpus] = useState(1000000000);
     const [mutualFundData, setMutualFundData] = useState({
         mutualFundName: '',
         entryLoad: 0,
@@ -24,15 +15,10 @@ export default function CreateMutualFund() {
     });
 
 
-    const togglePasswordVisibility = () => {
-        setPasswordVisible(true);
-    };
-
     useEffect(() => {
         async function fetchMutualFunds() {
             try {
                 const response = await getAllStocks();
-                setStocksList(response);
             } catch (error) {
                 console.error(error);
             }
@@ -40,78 +26,39 @@ export default function CreateMutualFund() {
         fetchMutualFunds();
     }, []);
 
-    const handleCheckBox = (event) => {
-        const fields = document.getElementsByClassName("checkbox-field")
-        if (event.target.checked) {
-            for (let field of fields) {
-                const withoutPrefix = field.id.substring("field-".length);
-                if (withoutPrefix === event.target.value) {
-                    field.disabled = false;
-                }
-            }
-        } else {
-            for (let field of fields) {
-                const withoutPrefix = field.id.substring("field-".length);
-                if (withoutPrefix === event.target.value) {
-                    field.disabled = true;
-                }
-            }
-        }
-    }
-
-    const handleAddStocks = () => {
-        setPasswordVisible(false);
-        const checkboxes = document.getElementsByClassName("form-check-input");
-        const fields = document.getElementsByClassName("checkbox-field");
-        const stocks = []
-        for (let i in checkboxes) {
-            if (checkboxes[i].checked) {
-                const obj = {
-                    stockId: checkboxes[i].value,
-                    weightage: parseFloat(fields[i].value) / 100
-                }
-                stocks.push(obj);
-            }
-        }
-
-        const validStocks = stocks.filter(element => {
-            return (element.weight > 0);
-        })
-
-        setAddStocks(prevAddStocks => [...prevAddStocks, ...validStocks]); // Update addStocks state
-
-        // Update mutualFundData state (if needed)
-        setMutualFundData(prevMutualFundData => ({
-            ...prevMutualFundData,
-            stocks: [...prevMutualFundData.stocks, ...validStocks],
-        }));
-    }
-
-
-    const handleFields = (event) => {
-        if (parseFloat(event.target.value) >= 100) {
-            event.target.value = 100;
-        }
-    }
-
-    const handleSubmit = () => {
-        const fundName = parseFloat(document.getElementById("fundName").value);
-        const entryLoad = parseFloat(document.getElementById("entryLoad").value);
-        const exitLoad = parseFloat(document.getElementById("exitLoad").value);
-        setMutualFundData(prev => ({
-            ...prev,
-            mutualFundName: fundName,
-            entryLoad: entryLoad,
-            exitLoad: exitLoad,
-        }))
-
-        console.log(mutualFundData);
-    }
-
     return (
         <div className='create-container'>
             <div className="input-container">
-                
+                <div className="left">
+                    <div className="fund-name field-entity">
+                        <label htmlFor="fundName">Enter Fund Name</label>
+                        <input type="text" name='fundName' id="fundName" className="field" />
+                    </div>
+                    <div className="entry-load field-entity">
+                        <label htmlFor="entryLoad">Entry Load</label>
+                        <input type="number" name='entryLoad' id="entryLoad" className="field" />
+                    </div>
+                    <div className="exit-field field-entity">
+                        <label htmlFor="exitLoad">Exit Load</label>
+                        <input type="number" name='exitLoad' id="exitLoad" className="field" />
+                    </div>
+                    <div className="expense-ratio field-entity">
+                        <label htmlFor="expenseRatio">Expense Ratio</label>
+                        <input type="number" name='expenseRatio' id="expenseRatio" className="field" />
+                    </div>
+                    <div className="submit field-entity">
+                        <button name='sumbit' id="submit" className="btn">Create Mutual Fund</button>
+                    </div>
+                </div>
+                <div className="right">
+                    <table className="data-table">
+                        <thead className="table-header">
+                            <tr></tr>
+                            <tr>Stock Name</tr>
+                            <tr>Weightage</tr>
+                        </thead>
+                    </table>
+                </div>
             </div>
         </div>
     )
